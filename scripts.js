e8ec197d22c1b930ce58a6714ef05540a2913473
serg-bolshakov <you@example.com>
2022-12-15 23:08:08 +0300

    попробовали скрипт

---
+++ scripts.js
@@ -0,0 +1,22 @@
+/*Получаем список элементов с заданным классом*/
+let acc = document.getElementsByClassName("accordion"); 
+
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Переключение между добавлением и удалением класса "active",
+    чтобы выделить кнопку, управляющую панелью */
+    this.classList.toggle("active");
+
+    /* Переключение между скрытием и отображением активной панели */
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+console.log('Всё в порядке?')
